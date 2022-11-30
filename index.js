7d0bae4cf9157f1fdafe99acf53f973792bdf4d6
zhuqinfei <1586859762@qq.com>
2022-12-01 02:44:29 +0800

    实现创建功能

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 
 const homedir=require('os').homedir()
 const home=process.env.HOME || homedir
+const fs=require('fs')
+const p=require('path')
+const dbpath=p.join(home,'.todo')
 
 module.exports.add=(title)=>{
-  console.log(home)
+  //读取之前的任务
+  fs.readFile(dbpath,{flag:'a+'},(error,data)=>{
+    if(error){
+      console.log(error)
+    }else{
+      let list
+      try{
+        list=JSON.parse(data.toString())
+      }catch(error2){
+        list=[]
+      }
+      console.log(list)
+      const task={
+        title:title,
+        done:false
+      }
+      list.push(task)
+      const string=JSON.stringify(list)
+      fs.writeFile(dbpath,string,(error3)=>{
+         if(error3){
+           console.log(error3)
+         }
+      })
+    }
+  })
+  //往里面添加一个title任务
+  //存储任务到文件
 }
