787f4c9acdd7f1541b6859da0656cc0b80b3d8c1
zhuqinfei <1586859762@qq.com>
2022-12-02 03:07:07 +0800

    封装优化代码

--- index.js
+++ index.js
@@ -14,78 +14,102 @@ module.exports.clear=async ()=>{
   await db.write([])
 }
 
+function markAsDone(list,index){
+  list[index].done=true
+  db.write(list)
+}
+function markAsUndone(list,index){
+  list[index].done=false
+  db.write(list)
+}
+function undateTitle(list,index){
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'title',
+      message: '新的标题',
+      default:list[index].title
+    }).then(answer=>{
+    list[index].title=answer.title
+    db.write(list)
+  })
+}
+function remove(list,index){
+  list.splice(index,1)
+  db.write(list)
+}
+
+function askForAction(list,index){
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'action',
+      message: '请选择操作',
+      choices: [
+        {name:'退出',value:'quit'},
+        {name:'已完成',value:'markAsDone'},
+        {name:'未完成',value:'markAsUndone'},
+        {name:'改标题',value:'undateTitle'},
+        {name:'删除',value:'remove'}
+      ]
+    }).then((answer2)=>{
+    switch(answer2.action){
+      case 'markAsDone':
+        markAsDone(list,index)
+        break
+      case 'markAsUndone':
+        markAsUndone(list,index)
+        break
+      case 'undateTitle':
+        undateTitle(list,index)
+        break
+      case 'remove':
+        remove(list,index)
+        break
+    }
+  })
+}
+
+function askForCreateTask(list){
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'title',
+      message: '输入任务标题',
+    }).then(answer=>{
+    list.push({
+      title:answer.title,
+      done:false
+    })
+    db.write(list)
+  })
+}
+
+function printTasks(list){
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'index',
+      message: '请选择你想操作的任务',
+      choices: [{name:'退出',value:'-1'},...list.map((task,index)=>{
+        return {name:`${task.done?'[x]':'[_]'}${index+1} - ${task.title}`,value:index}
+      }),{name:'+创建任务',value:'-2'}]
+    })
+    .then((answers) => {
+      const index=parseInt(answers.index)
+      if(index>=0){
+        //选中一个任务
+        askForAction(list,index)
+      }else if(index===-2){
+        //创建任务
+        askForCreateTask(list)
+      }
+    });
+}
 
 module.exports.showAll=async ()=>{
   //读取之前的任务
   const list=await db.read()
   //打印之前的任务
-  inquirer
-    .prompt({
-        type: 'list',
-        name: 'index',
-        message: '请选择你想操作的任务',
-        choices: [{name:'退出',value:'-1'},...list.map((task,index)=>{
-          return {name:`${task.done?'[x]':'[_]'}${index+1} - ${task.title}`,value:index}
-        }),{name:'+创建任务',value:'-2'}]
-      })
-    .then((answers) => {
-      const index=parseInt(answers.index)
-      if(index>=0){
-        //选中一个任务
-        inquirer
-          .prompt({
-            type: 'list',
-            name: 'action',
-            message: '请选择操作',
-            choices: [
-              {name:'退出',value:'quit'},
-              {name:'已完成',value:'markAsDone'},
-              {name:'未完成',value:'markAsUndone'},
-              {name:'改标题',value:'undateTitle'},
-              {name:'删除',value:'remove'}
-            ]
-          }).then((answer2)=>{
-            switch(answer2.action){
-              case 'markAsDone':
-                list[index].done=true
-                db.write(list)
-                break
-              case 'markAsUndone':
-                list[index].done=false
-                db.write(list)
-                break
-              case 'undateTitle':
-                inquirer
-                  .prompt({
-                    type: 'input',
-                    name: 'title',
-                    message: '新的标题',
-                    default:list[index].title
-                  }).then(answer=>{
-                    list[index].title=answer.title
-                    db.write(list)
-                })
-                break
-              case 'remove':
-                list.splice(index,1)
-                db.write(list)
-                break
-            }
-        })
-      }else if(index===-2){
-        //创建任务
-        inquirer
-          .prompt({
-            type: 'input',
-            name: 'title',
-            message: '输入任务标题',
-          }).then(answer=>{
-          list.push({
-            title:answer.title,
-            done:false
-          })
-          db.write(list)
-        })
-      }
-    });
+  printTasks(list)
 }
