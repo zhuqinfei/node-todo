4ccb1fe356438baeb0865dc4b13231a5a24371a7
zhuqinfei <1586859762@qq.com>
2022-12-02 01:05:29 +0800

    完成所有功能

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const db=require('./db.js')
+const inquirer=require('inquirer')
 
 module.exports.add=async (title)=>{
   //读取之前的任务
@@ -18,7 +19,73 @@ module.exports.showAll=async ()=>{
   //读取之前的任务
   const list=await db.read()
   //打印之前的任务
-  list.forEach((task,index)=>{
-    console.log(`${task.done?'[x]':'[_]'}${index+1} - ${task.title}`)
-  })
+  inquirer
+    .prompt({
+        type: 'list',
+        name: 'index',
+        message: '请选择你想操作的任务',
+        choices: [{name:'退出',value:'-1'},...list.map((task,index)=>{
+          return {name:`${task.done?'[x]':'[_]'}${index+1} - ${task.title}`,value:index}
+        }),{name:'+创建任务',value:'-2'}]
+      })
+    .then((answers) => {
+      const index=parseInt(answers.index)
+      if(index>=0){
+        //选中一个任务
+        inquirer
+          .prompt({
+            type: 'list',
+            name: 'action',
+            message: '请选择操作',
+            choices: [
+              {name:'退出',value:'quit'},
+              {name:'已完成',value:'markAsDone'},
+              {name:'未完成',value:'markAsUndone'},
+              {name:'改标题',value:'undateTitle'},
+              {name:'删除',value:'remove'}
+            ]
+          }).then((answer2)=>{
+            switch(answer2.action){
+              case 'markAsDone':
+                list[index].done=true
+                db.write(list)
+                break
+              case 'markAsUndone':
+                list[index].done=false
+                db.write(list)
+                break
+              case 'undateTitle':
+                inquirer
+                  .prompt({
+                    type: 'input',
+                    name: 'title',
+                    message: '新的标题',
+                    default:list[index].title
+                  }).then(answer=>{
+                    list[index].title=answer.title
+                    db.write(list)
+                })
+                break
+              case 'remove':
+                list.splice(index,1)
+                db.write(list)
+                break
+            }
+        })
+      }else if(index===-2){
+        //创建任务
+        inquirer
+          .prompt({
+            type: 'input',
+            name: 'title',
+            message: '输入任务标题',
+          }).then(answer=>{
+          list.push({
+            title:answer.title,
+            done:false
+          })
+          db.write(list)
+        })
+      }
+    });
 }
